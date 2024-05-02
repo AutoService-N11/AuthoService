ce41114a38ac28f3abd1301dc0d89865941f76be
Ozodbek <145344998+OlimjonovOzodbek@users.noreply.github.com>
2024-05-02 18:43:32 +0500

    Full crud Car Records

---
+++ src/AutoService.Application/UseCases/CarCases/CarRecordCases/Queries/GetAllCarRecordQuery.cs
@@ -0,0 +1,14 @@
+﻿using AutoService.Domain.Entities.ViewModels.CarRecordViewModels;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoService.Application.UseCases.CarCases.CarRecordCases.Queries
+{
+    public class GetAllCarRecordQuery: IRequest<IEnumerable<CarRecordViewModel>>
+    {
+    }
+}
