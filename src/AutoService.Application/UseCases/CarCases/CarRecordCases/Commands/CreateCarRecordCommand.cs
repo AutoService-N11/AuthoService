6da361707d0b3ac47bc2d19a8f61fe19374c48f8
Ozodbek <145344998+OlimjonovOzodbek@users.noreply.github.com>
2024-05-02 18:31:42 +0500

    Create Delete for Car Record

--- src/AutoService.Application/UseCases/CarCases/CarRecordCases/Commands/CreateCarRecordCommand.cs
+++ src/AutoService.Application/UseCases/CarCases/CarRecordCases/Commands/CreateCarRecordCommand.cs
@@ -1,22 +1,20 @@
+﻿using AutoService.Domain.Entities.Models;
+using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
-namespace AutoService.Domain.Entities.Models.CarModels
+namespace AutoService.Application.UseCases.CarCases.CarRecordCases.Commands
 {
-    public class CarRecord
+    public class CreateCarRecordCommand: IRequest<ResponceModel>
     {
-        public Guid Id { get; set; } = Guid.NewGuid();
         public Guid UserCarId { get; set; }
         public DateTimeOffset createdDate { get; set; } = DateTimeOffset.Now;
         public int Probeg { get; set; }
         public string RecordTask { get; set; }
         public string Comment { get; set; }
         public string Price { get; set; }
-
-        public virtual UserCar UserCar { get; set; }
     }
 }
---
+++ src/AutoService.Application/UseCases/CarCases/CarRecordCases/Handlers/CreateCarRecordCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using AutoService.Application.Abstractions;
+using AutoService.Application.UseCases.CarCases.CarRecordCases.Commands;
+using AutoService.Domain.Entities.Models;
+using AutoService.Domain.Entities.Models.CarModels;
+using AutoService.Domain.Entities.ViewModels.CarRecordViewModels;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoService.Application.UseCases.CarCases.CarRecordCases.Handlers
+{
+    public class CreateCarRecordCommandHandler : IRequestHandler<CreateCarRecordCommand, ResponceModel>
+    {
+        private readonly IAppDbContext _context;
+
+        public CreateCarRecordCommandHandler(IAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResponceModel> Handle(CreateCarRecordCommand request, CancellationToken cancellationToken)
+        {
+            var result = new CarRecord()
+            {
+                UserCarId = request.UserCarId,
+                createdDate = request.createdDate,
+                Probeg = request.Probeg,
+                RecordTask = request.RecordTask,
+                Comment = request.Comment,
+                Price = request.Price,
+            };
+            return new ResponceModel
+            {
+                Message = "CarRecord created",
+                StatusCode = 200,
+
+            };
+        }
+    }
+}
