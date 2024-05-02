725c69309a32b24885155316b00bd44edde3d445
Ozodbek <145344998+OlimjonovOzodbek@users.noreply.github.com>
2024-05-02 14:37:30 +0500

    seatbrand create done

---
+++ src/AutoService.Application/UseCases/ShopCases/CarSeatBrandCases/Commands/CreateCarSeatBrandCommand.cs
@@ -0,0 +1,15 @@
+﻿using AutoService.Domain.Entities.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoService.Application.UseCases.ShopCases.CarSeatBrandCases.Commands
+{
+    public class CreateCarSeatBrandCommand:IRequest<ResponceModel>
+    {
+        public string Name {  get; set; }
+    }
+}
---
+++ src/AutoService.Application/UseCases/ShopCases/CarSeatBrandCases/Handlers/CreateCarSeatBrandHandler.cs
@@ -0,0 +1,48 @@
+﻿using AutoService.Application.Abstractions;
+using AutoService.Application.UseCases.ShopCases.CarSeatBrandCases.Commands;
+using AutoService.Domain.Entities.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoService.Application.UseCases.ShopCases.CarSeatBrandCases.Handlers
+{
+    public class CreateCarSeatBrandHandler : IRequestHandler<CreateCarSeatBrandCommand, ResponceModel>
+    {
+        private readonly IAppDbContext _context;
+
+        public CreateCarSeatBrandHandler(IAppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<ResponceModel> Handle(CreateCarSeatBrandCommand request, CancellationToken cancellationToken)
+        {
+            var res = await _context.CarSeatBrands.FirstOrDefaultAsync(x => x.Name == request.Name);
+
+            if (res == null)
+            {
+                res.Name = request.Name;
+
+                await _context.CarSeatBrands.AddAsync(res);
+                await _context.SaveChangesAsync(cancellationToken);
+
+                return new ResponceModel
+                {
+                    Message = "You succesfully added a new brand!",
+                    StatusCode = 200,
+                    IsSuccess = true
+                };
+            }
+
+            return new ResponceModel
+            {
+                Message = "This brand already exists",
+                StatusCode = 409
+            };
+        }
+    }
+}
